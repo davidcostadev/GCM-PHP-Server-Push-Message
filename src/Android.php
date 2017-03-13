32ec8145a57a79f252368cab0236a9624a312cd2
David Costa <davidcostadev@gmail.com>
2017-03-13 08:22:40 -0300

    melhorando funcao de envio para android

--- src/Android.php
+++ src/Android.php
@@ -5,14 +5,44 @@ namespace NotificationPhonegap;
 // use GCMPushMessage;
 
 
-Class Android {
+final class Android {
 
+    private static $_instance;
 
+    private $Api;
 
-	public static function test() {
+    public function __construct () {
+        $this->Push = new GCMPushMessage();
+    }
 
+    private static function instance() {
+        if(is_null(self::$_instance)) {
+            self::$_instance = new self();
+        }
 
+        return self::$_instance;
+    }
 
-		var_dump($this);
-	}
+    public function setDevices($key) {
+        $this->Push->setDevices($key);
+    }
+
+    public static function setKey($key) {
+        self::instance()
+        ->Push->setKey($key);
+    }
+
+    public static function recipients(array $devices) {
+        self::instance()
+            ->setDevices($devices);
+    }
+
+    public static function simple($message) {
+        $self = self::instance();
+
+        $response = $self->Push->send($message);
+
+        echo $response;
+
+    }
 }
